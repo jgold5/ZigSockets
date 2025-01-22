e91621d25108b7b061b741b90b0106eb3b596ce4
Justin Gold <jgold885@gmail.com>
2025-01-21 19:08:53 -0500

    moving listen on port out of main loop

--- src/server.zig
+++ src/server.zig
@@ -5,21 +5,13 @@ const gpaConfig = std.heap.GeneralPurposeAllocatorConfig;
 const arrayList = std.ArrayList;
 
 pub fn start_server(name: []const u8, port: u16) !void {
-    while (true) {
-        const stream = try open_socket(name, port);
-        defer stream.close();
-        std.debug.print("Connected\n", .{});
-        try handle_conn(stream);
-        std.debug.print("Disconnected\n", .{});
-    }
-}
-
-fn open_socket(name: []const u8, port: u16) !net.Stream {
     const addr = try net.Address.parseIp4(name, port);
     var server = try net.Address.listen(addr, .{});
     defer server.deinit();
-    const conn = try server.accept();
-    return conn.stream;
+    while (true) {
+        try handle_conn(&server);
+        std.debug.print("Disconnected\n", .{});
+    }
 }
 
 fn send_response(stream: net.Stream) void {
@@ -33,27 +25,29 @@ fn send_response(stream: net.Stream) void {
     };
 }
 
-fn handle_conn(stream: net.Stream) !void {
+fn handle_conn(server: *net.Server) !void {
+    const conn = try server.accept();
+    std.debug.print("Connected\n", .{});
+    const stream = conn.stream;
+    defer stream.close();
     const default_config = gpaConfig{};
     var initializedGpa = gpa(default_config){};
     const alloc = initializedGpa.allocator();
     const read_buf = try alloc.alloc(u8, 1024);
     const write_buf = try alloc.alloc(u8, 1024);
     var al = arrayList(u8).init(alloc);
-    while (true) {
-        const bytes_read = try stream.read(read_buf);
-        if (bytes_read == 0) {
-            break;
-        }
-        const response_header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: ";
-        const res = try std.fs.cwd().readFile("src/index.html", write_buf);
-        const len_as_str = try std.fmt.allocPrint(alloc, "{d}", .{res.len});
-        _ = try al.appendSlice(response_header);
-        _ = try al.appendSlice(len_as_str);
-        _ = try al.appendSlice("\r\n\r\n");
-        _ = try al.appendSlice(res);
-        _ = try stream.write(try al.toOwnedSlice());
-        std.debug.print("Read: {} bytes\n", .{bytes_read});
-        std.debug.print("Read: {s}\n", .{read_buf[0..bytes_read]});
+    const bytes_read = try stream.read(read_buf);
+    if (bytes_read == 0) {
+        return;
     }
+    const response_header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: ";
+    const res = try std.fs.cwd().readFile("src/index.html", write_buf);
+    const len_as_str = try std.fmt.allocPrint(alloc, "{d}", .{res.len});
+    _ = try al.appendSlice(response_header);
+    _ = try al.appendSlice(len_as_str);
+    _ = try al.appendSlice("\r\n\r\n");
+    _ = try al.appendSlice(res);
+    _ = try stream.write(try al.toOwnedSlice());
+    std.debug.print("Read: {} bytes\n", .{bytes_read});
+    std.debug.print("Read: {s}\n", .{read_buf[0..bytes_read]});
 }
